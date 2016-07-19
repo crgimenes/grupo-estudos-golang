580197ab0e7faa320c148fb5c7c573711ca11112
Cesar Gimenes <crg@crg.eti.br>
2016-07-18 22:14:57 -0300

    pwm

---
+++ pwm/pwm.go
@@ -0,0 +1,54 @@
+package main
+
+/*
+#cgo LDFLAGS: -lmraa pwm.a
+#include "pwm.h"
+*/
+import "C"
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"time"
+)
+
+var duty float32 = 0.5  // guarda o ciclo de trabalho
+var delta float32 = 1.0 // variação do ciclo de trabalho
+
+func main() {
+
+	keeprunning := true
+
+	go func() {
+		sc := make(chan os.Signal, 1)
+		signal.Notify(sc, os.Interrupt)
+		// espera pelo sinal
+		<-sc
+		keeprunning = false
+	}()
+
+	C.config()
+
+	for keeprunning {
+
+		if duty >= 1 {
+			duty = 1      // Intencidade do LED
+			delta = -0.05 // Diminui a variação do ciclo de trabalho
+		} else if duty <= 0 {
+			duty = 0      // Intencidade do LED
+			delta = +0.05 // Aumenta a variação do ciclo de trabalho
+		}
+
+		// Ajusta o ciclo de trabalho
+		C.writePWM(C.float(duty))
+
+		time.Sleep(50000 * time.Microsecond)
+
+		duty = duty + delta
+
+		fmt.Printf("ciclo de trabalho: %f\r\n", duty)
+	}
+
+	C.stopPWM()
+}
---
+++ pwm/pwm.h
@@ -0,0 +1,8 @@
+#ifndef __PWM_H__
+#define __PWM_H__
+
+void config(void);
+void writePWM(float duty);
+void stopPWM(void);
+
+#endif
