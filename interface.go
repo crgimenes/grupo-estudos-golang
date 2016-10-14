15ee18aead76266235a7f41cc6cd69789d31faa5
Guilherme Paixão <guilhermee.paixaoo@gmail.com>
2016-10-14 09:58:08 -0300

    Criando arquivo .go para demonstração

---
+++ interface.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type (
+	Ser interface {
+		Respirar() bool
+	}
+
+	Humano   struct{}
+	Cachorro struct{}
+	Pedra    struct{}
+)
+
+func (h Humano) Respirar() bool {
+	return false
+}
+
+func (c Cachorro) Respirar() bool {
+	return true
+}
+
+func VerificandoSeEstaVivo(s Ser) {
+	if s.Respirar() {
+		fmt.Println("Está vivo.")
+	} else {
+		fmt.Println("Não está vivo.")
+	}
+}
+
+func main() {
+	humano := Humano{}
+	cachorro := Cachorro{}
+	// pedra := Pedra{}
+
+	VerificandoSeEstaVivo(humano)
+	VerificandoSeEstaVivo(cachorro)
+	// VerificandoSeEstaVivo(pedra)
+}
