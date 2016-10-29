144d0db082d24149f21723e17ff2e8954c90ce76
Osni Oliveira <ooliveir@redhat.com>
2016-10-29 15:27:59 -0200

    Adiciona exemplo de retorno assinado

--- funcoes.go
+++ funcoes.go
@@ -4,12 +4,20 @@ import (
 	"fmt"
 )
 
+// Retorno simples
 func sum(x int, y int) int {
 	return x + y
 }
 
+// Retorno duplo
 func swap(x string, y string) (string, string) {
 	return y, x
+}
+
+// Retorno assinado
+func div(x, y int) (result int) {
+	result = x / y
+	return
 }
 
 // função que recebe uma função como parâmetro
@@ -25,6 +33,9 @@ func main() {
 
 	r1, r2 := swap("A", "B")
 	fmt.Printf("troca A, B = %v, %v\r\n", r1, r2)
+
+	resultadoDivisao := div(10, 5)
+	fmt.Printf("divisao 10 / 5 = %v\r\n", resultadoDivisao)
 
 	// função anonima que vamos passar para printFunc
 	f := func(v string) string {
