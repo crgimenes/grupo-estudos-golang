41681e13c9fc525de593e328faf6bb744088839f
Cesar Gimenes <crg@crg.eti.br>
2017-03-16 18:40:20 -0300

    remove cria_assets.sh

--- cria_assets.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -f ./assets/*.html
-find ./ -depth -name "*.md" -exec sh -c 'markdown "$1">"./assets/${1%.md}.html"' _ {} \;
-sed -i -e 's/.md/.html/g' ./assets/*.html
-rm -f ./assets/*.html-e
-
-
