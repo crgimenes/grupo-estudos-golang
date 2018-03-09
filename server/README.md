f1e9063ec5fb1aac38a584869be72d0747c42d8e
Cesar Gimenes <crg@crg.eti.br>
2018-03-09 14:55:13 -0300

    servidor simples para testes de clientes

---
+++ server/README.md
@@ -0,0 +1,12 @@
+# HTTP Server
+
+Este é um pequeno servidor HTTP usado para testes de clientes.
+
+Ele abre a porta 8080 e ecoa tudo que for enviado para ele via terminal.
+
+Para executar basta
+
+```console
+go get ./...
+go run main.go
+```
---
+++ server/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/nuveo/log"
+)
+
+func closer(body io.Closer) {
+	err := body.Close()
+	if err != nil {
+		log.Errorln(err)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Errorln(err)
+	}
+	defer closer(r.Body)
+	fmt.Println(string(b))
+	w.Write([]byte("ok\n"))
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
