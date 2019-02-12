721db9c9148ec9dd5c495859e8ae09b0f8062278
Cesar Gimenes <crg@crg.eti.br>
2019-02-12 21:21:39 -0200

    implementa quine em go

---
+++ quine/main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	aspa := string(96)
+	print(codigo, aspa, codigo, aspa)
+}
+
+const codigo = `package main
+
+func main() {
+	aspa := string(96)
+	print(codigo, aspa, codigo, aspa)
+}
+
+const codigo = `
