c9b85643d44fcf9f63773a819068ed68310f272d
Cesar Gimenes <crg@crg.eti.br>
2020-02-13 16:29:46 -0300

    add main and test file

---
+++ interface/e1/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"database/database/bbolt"
+	"fmt"
+	"log"
+)
+
+func main() {
+
+	db, err := bbolt.New("teste.db")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = db.Update("jobs", "testKey", []byte("test 123"))
+	if err != nil {
+		log.Fatalf("Uodate(): %v\n", err)
+	}
+
+	b, err := db.View("jobs", "testKey")
+	if err != nil {
+		log.Fatalf("View(): %v\n", err)
+	}
+	fmt.Println("retorno:", string(b))
+
+}
