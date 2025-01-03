43df3e78e54df083714ed7ac3e727d9e7034ab95
crgimenes <crg@crg.eti.br>
2025-01-02 23:27:07 -0300

    exemplo de redução de privilégios

---
+++ reducao_de_privilegios/README.md
@@ -0,0 +1,6 @@
+# Redução de privilégios
+
+Demonstra o uso de syscall.Setuid e syscall.Setgid para reduzir os privilégios de um programa.
+
+Redução de privilégios é uma técnica de segurança que consiste em executar um programa com o minimo de privilégios necessários para a execução da tarefa. Isso é feito para limitar o impacto de um ataque bem sucedido.
+
---
+++ reducao_de_privilegios/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"strconv"
+	"syscall"
+)
+
+func main() {
+	if os.Getuid() != 0 {
+		fmt.Println("run as root")
+		return
+	}
+
+	fmt.Printf(
+		"running as uid: %v, gid: %v\n",
+		os.Getuid(),
+		os.Getgid(),
+	)
+
+	// pega o usuário nobody
+	u, err := user.Lookup("nobody")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	uid, _ := strconv.Atoi(u.Uid)
+	gid, _ := strconv.Atoi(u.Gid)
+
+	// ajusta o id do usuário
+	err = syscall.Setgid(gid)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// ajusta o id do grupo
+	err = syscall.Setuid(uid)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf(
+		"running as uid: %v, gid: %v\n",
+		os.Getuid(),
+		os.Getgid(),
+	)
+}
