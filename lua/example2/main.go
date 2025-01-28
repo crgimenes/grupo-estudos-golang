6f6a672152b1889cbd2884f0b36ece2927dc0d41
crgimenes <crg@crg.eti.br>
2025-01-28 01:51:47 -0300

    improves the example return

--- lua/example2/main.go
+++ lua/example2/main.go
@@ -7,28 +7,32 @@ import (
 func concat(l *lua.LState) int {
 	a := l.ToString(1)
 	b := l.ToString(2)
-	res := lua.LString(a + b)
-	l.Push(res)
+	ret := a + b
+	l.Push(lua.LString(ret))
 	return 1
 }
 
 func sum(l *lua.LState) int {
 	a := l.ToInt(1)
 	b := l.ToInt(2)
-	res := lua.LNumber(a + b)
-	l.Push(res)
+	ret := a + b
+	l.Push(lua.LNumber(ret))
 	return 1
 }
 
 func main() {
 	l := lua.NewState()
 	defer l.Close()
+
 	l.SetGlobal("concat", l.NewFunction(concat))
 	l.SetGlobal("sum", l.NewFunction(sum))
+
 	err := l.DoString(`
-		print("teste1 + teste2: " .. concat("test1","test2"))
+
+		print("concat(\"test1\",\"test2\"): " .. concat("test1","test2"))
 		print("sum(2,2): " .. sum(2,2))
-		`)
+
+	`)
 	if err != nil {
 		panic(err)
 	}
