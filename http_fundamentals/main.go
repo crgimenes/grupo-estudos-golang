663342fdcb8376623a75741b46d4c3b6d309c0a4
Akari <fbbx@proton.me>
2026-07-14 00:47:36 -0300

    docs: expand HTTP fundamentals topic

--- http_fundamentals/main.go
+++ http_fundamentals/main.go
@@ -11,6 +11,7 @@ type message struct {
 
 func HealthHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", http.MethodGet)
 		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
